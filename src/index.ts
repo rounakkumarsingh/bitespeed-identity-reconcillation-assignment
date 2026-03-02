165f935fff48fef0903ace9f76661ddd95e07c2d
Rounak Kumar Singh <rounak.official.2@gmail.com>
2026-03-03 03:55:23 +0530

    init setup and data insert

---
+++ src/index.ts
@@ -0,0 +1,104 @@
+import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
+import { SQL, sql } from "bun";
+import { z } from "zod";
+
+/* ================================
+   DB
+================================ */
+const db = new SQL("postgres://postgres@localhost:5432/postgres");
+
+/* ================================
+   App
+================================ */
+const app = new Hono();
+
+/* ================================
+   Validation
+================================ */
+const inputSchema = z
+    .object({
+        phoneNumber: z.string().optional(),
+        email: z.email().optional(),
+    })
+    .refine(
+        (data) => data.phoneNumber || data.email,
+        "Either phoneNumber or email is required",
+    );
+
+type IdentifyInput = z.infer<typeof inputSchema>;
+
+const outputSchema = z.object({
+    primaryContactId: z.number(),
+    emails: z.array(z.email()),
+    phoneNumbers: z.array(z.string()),
+    secondaryContactIds: z.array(z.number()),
+});
+
+type ContactOutput = z.infer<typeof outputSchema>;
+/* ================================
+   Data Layer
+================================ */
+async function findContacts({
+    phoneNumber,
+    email,
+}: IdentifyInput): Promise<ContactOutput> {
+    if (!phoneNumber && !email) {
+        throw new Error("Invalid data format");
+    }
+
+    const emailFilter =
+        email !== undefined ? sql`email = ${email}` : sql`FALSE`;
+
+    const phoneFilter =
+        phoneNumber !== undefined
+            ? sql`phone_number = ${phoneNumber}`
+            : sql`FALSE`;
+
+    const rows = await db`
+    SELECT *
+    FROM contact
+    WHERE ${emailFilter} OR ${phoneFilter}
+  `.values();
+
+    if (rows.length === 0) {
+        const [contact] = await db`
+        INSERT INTO contact (email, phone_number, link_precedence)
+        VALUES (${email}, ${phoneNumber}, 'primary')
+        RETURNING *
+        `;
+        return {
+            primaryContactId: contact.id,
+            phoneNumbers: [contact["phone_number"]],
+            emails: [contact["email"]],
+            secondaryContactIds: [],
+        };
+    }
+}
+/* ================================
+   Routes
+================================ */
+app.post("/identify", zValidator("json", inputSchema), async (c) => {
+    try {
+        const data = c.req.valid("json");
+
+        const contacts = await findContacts(data);
+
+        return c.json({
+            success: true,
+            data: contacts,
+        });
+    } catch (err) {
+        console.error(err);
+
+        return c.json(
+            {
+                success: false,
+                error: "Internal Server Error",
+            },
+            500,
+        );
+    }
+});
+
+export default app;
